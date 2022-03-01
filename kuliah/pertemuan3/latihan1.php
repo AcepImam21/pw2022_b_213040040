a47ac8643c0723376512f8906ef89bd9802016e3
AcepImam21 <Acepimam19@gmail.com>
2022-03-01 22:09:30 +0700

    mengerjakan latihan di pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,41 @@
+<?php
+// pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+for( $i = 0; $i < 5; $i++ ) {
+    echo "Hello World! <br>";
+}
+echo '<hr>';
+
+// $i = 10;
+// while( $i < 5) {
+//     echo "Hello World! <br>";
+//     $i++;
+// }
+
+$i = 0;
+while( $i < 5) {
+    echo "Hello World! <br>";
+    $i++;
+}
+
+echo '<hr>';
+
+$i = 10;
+do {
+    echo "Hello World! <br>";
+$i++;    
+} while( $i < 5 )
+
+
+
+
+
+
+
+
+
+?>
